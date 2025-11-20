fb7a245aa251c8670e5e38f5786cec1c6053b045
AxelPrince8 <honer.irtaral@gmail.com>
2025-11-20 18:35:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,16 +2,24 @@ import express from "express";
 import fetch from "node-fetch";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
+// Fix __dirname in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 app.use(cookieParser());
-app.use(express.static("public"));
+
+// Public folder serve (index.html, privacy.html, delete.html)
+app.use(express.static(path.join(__dirname, "public")));
 
 const FB_APP_ID = process.env.FB_APP_ID;
 const FB_APP_SECRET = process.env.FB_APP_SECRET;
-const REDIRECT_URI = process.env.REDIRECT_URI; // e.g. https://yourapp.onrender.com/callback
+const REDIRECT_URI = process.env.REDIRECT_URI;
 
 // Step 1: Send user to Facebook Login
 app.get("/login", (req, res) => {
@@ -22,7 +30,7 @@ app.get("/login", (req, res) => {
   res.redirect(url);
 });
 
-// Step 2: Facebook sends ?code=XXX
+// Step 2: Facebook sends back code
 app.get("/callback", async (req, res) => {
   const { code } = req.query;
 
@@ -33,13 +41,23 @@ app.get("/callback", async (req, res) => {
   const tokenResponse = await fetch(tokenURL);
   const tokenData = await tokenResponse.json();
 
-  // tokenData.access_token is the token
   res.send(`
     <h1>Facebook Token:</h1>
-    <textarea style="width:100%;height:150px;">${tokenData.access_token}</textarea>
+    <textarea style="width:100%;height:150px;">${tokenData.access_token || "Error: " + JSON.stringify(tokenData)}</textarea>
     <br><br>
     <a href="/">Go Back</a>
   `);
 });
 
+// Privacy policy page
+app.get("/privacy", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "privacy.html"));
+});
+
+// Data deletion page
+app.get("/delete", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "delete.html"));
+});
+
+// Run server
 app.listen(3000, () => console.log("Server running on 3000"));
